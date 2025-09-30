300e309d018dcd6c8f2a7ec213aa2e92f935dfe3
Péter Rácz <petermartonracz@gmail.com>
2025-09-30 21:50:22 +0200

    konfint fixed

--- script/R2/lesson2konfint.R
+++ script/R2/lesson2konfint.R
@@ -1,4 +1,7 @@
+set.seed(1337)
+
 library(tidyverse)
+library(glue)
 library(ggthemes)
 
 generate_correlated_base = function(n, rho, noise_sd = 1) {
@@ -15,7 +18,7 @@ generate_correlated_base = function(n, rho, noise_sd = 1) {
   return(data.frame(x = x, y = y))
 }
 
-d = generate_correlated_base(n = 1000, rho = 0.7, noise_sd = 65)
+d = generate_correlated_base(n = 1000, rho = 0.7, noise_sd = 20)
 
 summary(lm(y ~ x, data = d))
 
@@ -29,7 +32,7 @@ my_d = d |>
 
 p1 = my_d |> 
   ggplot(aes(x,y)) +
-  geom_point() +
+  geom_point(alpha = .25) +
   theme_void()
 
 built = ggplot_build(p1)
@@ -38,7 +41,7 @@ y_range = built$layout$panel_params[[1]]$y.range
 
 p2 = my_d |> 
   ggplot(aes(x,y)) +
-  geom_point() +
+  geom_point(alpha = .25) +
   theme_void() +
   geom_smooth(method = 'lm', se = F)
 
@@ -52,28 +55,28 @@ my_palette = c('#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00',
 dir.create("~/Downloads/konfint", showWarnings = FALSE)
 
 # Save first two plots
-ggsave("~/Downloads/konfint/p1.pdf", p1, width = 4, height = 4)
-ggsave("~/Downloads/konfint/p2.pdf", p2, width = 4, height = 4)
+ggsave("~/Downloads/konfint/p1.pdf",  bg = "white", p1, width = 4, height = 4)
+ggsave("~/Downloads/konfint/p2.pdf",  bg = "white", p2, width = 4, height = 4)
 
 # Counter for filenames
 plot_num = 3
 
 # First loop - highlight each subject
-for(i in 1:10) {
+for(i in 1:20) {
   p = my_d |> 
     mutate(my_colour = s_id == i) |> 
     ggplot(aes(x, y, colour = my_colour)) +
-    geom_point() +
+    geom_point(alpha = .5) +
     guides(colour = 'none') +
     theme_void() +
     scale_colour_manual(values = c('lightgrey', my_palette[i]))
   
-  ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p, width = 4, height = 4)
+  ggsave(plot = p, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
   plot_num = plot_num + 1
 }
 
 # Second loop - individual subject regressions
-for(i in 1:10) {
+for(i in 1:20) {
   p = my_d |> 
     filter(s_id == i) |> 
     ggplot(aes(x, y)) +
@@ -82,7 +85,7 @@ for(i in 1:10) {
     theme_void() +
     coord_cartesian(xlim = x_range, ylim = y_range)
   
-  ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p, width = 4, height = 4)
+  ggsave(plot = p, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
   plot_num = plot_num + 1
 }
 
@@ -94,58 +97,78 @@ p3 = my_d |>
   guides(colour = 'none') +
   coord_cartesian(xlim = x_range, ylim = y_range)
 
+ggsave(plot = p3, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
+
 p4 = my_d |> 
   ggplot(aes(x, y, colour = as.factor(s_id))) +
   geom_smooth(method = 'lm', se = F) +
   theme_void() +
   guides(colour = 'none') +
-  coord_cartesian(xlim = x_range, ylim = c(0.2,0.7))
+  coord_cartesian(xlim = x_range, ylim = y_range)
+
+ggsave(plot = p4, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
 
 p5 = my_d |> 
+  filter(s_id == 2) |> 
   ggplot(aes(x, y)) +
   geom_smooth(method = 'lm') +
   theme_void() +
   guides(colour = 'none') +
-  coord_cartesian(xlim = x_range, ylim = c(0.2,0.7))
+  coord_cartesian(xlim = x_range, ylim = y_range)
+
+ggsave(plot = p5, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
 
 p6 = my_d |> 
+  filter(s_id == 2) |> 
   ggplot(aes(x, y)) +
-  geom_smooth(method = 'lm', level = 0.99) +
+  geom_smooth(method = 'lm', level = .999) +
   theme_void() +
   guides(colour = 'none') +
-  coord_cartesian(xlim = x_range, ylim = c(0.2,0.7))
+  coord_cartesian(xlim = x_range, ylim = y_range)
+
+ggsave(plot = p6, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
 
 p7 = my_d |> 
+  filter(s_id == 2) |> 
   ggplot(aes(x, y)) +
-  geom_smooth(method = 'lm', level = 0.999) +
+  geom_smooth(method = 'lm', level = .5) +
   theme_void() +
   guides(colour = 'none') +
-  coord_cartesian(xlim = x_range, ylim = c(0.2,0.7))
+  coord_cartesian(xlim = x_range, ylim = y_range)
+
+ggsave(plot = p7, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
 
 p8 = my_d |> 
+  filter(s_id == 2) |> 
   ggplot(aes(x, y)) +
-  geom_smooth(method = 'lm', level = 0.9) +
+  geom_smooth(method = 'lm', level = .5) +
+  geom_smooth(method = 'lm', level = .95) +
+  geom_smooth(method = 'lm', level = .999) +
   theme_void() +
   guides(colour = 'none') +
-  coord_cartesian(xlim = x_range, ylim = c(0.2,0.7))
+  coord_cartesian(xlim = x_range, ylim = y_range)
 
-p9 = my_d |> 
+ggsave(plot = p8, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
+
+p9 = p5 +
+  geom_smooth(data = my_d, aes(x, y), se = FALSE, color = "red")
+
+ggsave(plot = p9, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
+plot_num = plot_num + 1
+
+p10 = my_d |> 
+  filter(s_id == 6) |> 
   ggplot(aes(x, y)) +
-  geom_smooth(method = 'lm', level = 0.5) +
+  geom_smooth(method = 'lm') +
   theme_void() +
   guides(colour = 'none') +
-  coord_cartesian(xlim = x_range, ylim = c(0.2,0.7))
+  coord_cartesian(xlim = x_range, ylim = y_range) +
+  geom_smooth(data = my_d, aes(x, y), se = FALSE, color = "red")
 
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p3, width = 4, height = 4)
-plot_num = plot_num + 1
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p4, width = 4, height = 4)
-plot_num = plot_num + 1
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p5, width = 4, height = 4)
-plot_num = plot_num + 1
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p6, width = 4, height = 4)
-plot_num = plot_num + 1
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p7, width = 4, height = 4)
-plot_num = plot_num + 1
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p8, width = 4, height = 4)
-plot_num = plot_num + 1
-ggsave(sprintf("~/Downloads/konfint/p%d.pdf", plot_num), p9, width = 4, height = 4)
+ggsave(plot = p10, filename = glue('~/Downloads/konfint/p{plot_num}.pdf'),  bg = "white", width = 4, height = 4)
