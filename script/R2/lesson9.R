c8a1ddfd5b8d3c1e2a0df7c3966ec58dec8ee417
Péter Rácz <petermartonracz@gmail.com>
2025-11-24 17:56:15 +0100

    lesson9

--- script/R2/lesson9.R
+++ script/R2/lesson9.R
@@ -1,9 +1,7 @@
-# -- starting questions -- #
+# -- intro -- #
 
-# what is the effect of vocabulary size and word familiarity?
-# is there an interaction?
-# what is the structure of the data?
-# what is the result of clustering?
+# https://mfviz.com/hierarchical-models/
+# https://m-clark.github.io/posts/2019-05-14-shrinkage-in-mixed-models/
 
 # -- setup -- #
 
@@ -11,173 +9,105 @@
 install.packages("lme4")
 install.packages("broom.mixed")
 
-# load the tidyverse, broom.mixed, performance, sjPlot, psych, janitor, and lme4 libraries
 library(tidyverse)
 library(broom.mixed)
 library(performance)
 library(sjPlot)
-library(psych)
-library(janitor)
 library(lme4)
 
-# load table from 'https://raw.githubusercontent.com/petyaracz/AdatmanipulacioModellezes/main/dat/R2/lesson9.tsv' and assign to d
-d <- read_tsv("https://raw.githubusercontent.com/petyaracz/AdatmanipulacioModellezes/main/dat/R2/lesson9.tsv")
+# -- read -- #
 
-d |> View
+d = read_tsv("https://raw.githubusercontent.com/petyaracz/AdatmanipulacioModellezes/main/dat/R2/lesson9.tsv")
 
-# take d, scale word familiarity and vocabulary size, assign to d
-d <- d |> mutate(scaled_word_familiarity = scale(word_familiarity), scaled_participant_vocabulary_size = scale(participant_vocabulary_size))
+keep1 = sample(unique(d$participant), 12)
+d$keep_participant = d$participant %in% keep1
+keep2 = sample(unique(d$word), 12)
+d$keep_word = d$word %in% keep2
 
-# -- eda: word familiarity -- #
+write_tsv(d, '~/Github/AdatManipulacioModellezes/dat/R2/lesson9.tsv')
 
-# take d, plot resp.rt ~ word_familiarity with points and a line
+# -- take a look -- #
+
+glimpse(d)
+
+unique(d$participant)
+unique(d$word)
+
+# -- visualise -- #
+
 d |> 
-  ggplot(aes(x = word_familiarity, y = resp.rt)) +
+  ggplot(aes(word_familiarity,log_rt)) +
   geom_point() +
-  geom_smooth(method = "lm")
+  geom_smooth(method = 'lm')
 
-# take d, plot resp.rt ~ scaled_word_familiarity with points and a line
 d |> 
-  ggplot(aes(x = scaled_word_familiarity, y = resp.rt)) +
+  ggplot(aes(participant_vocabulary_size,log_rt)) +
   geom_point() +
-  geom_smooth(method = "lm")
-
-# -- eda: word familiarity and vocabulary size -- #
-
-# take d, sample 10 participants, filter data for these ten participants, plot resp.rt ~ scaled_word_familiarity with points and a line
+  geom_smooth(method = 'lm')
 
 
-# not quite
+# -- participant-level effects -- #
 
-# take d, sample 10 participant ids, assign to ids
-ids = d |> 
-  distinct(participant) |>
-  sample_n(10)
-
-# take d, filter for participant ids in ids, plot resp.rt ~ scaled_word_familiarity with points and a line
 d |> 
-  filter(participant %in% ids$participant) |>
-  ggplot(aes(x = scaled_word_familiarity, y = resp.rt)) +
+  filter(keep_participant) |> 
+  ggplot(aes(word_familiarity,log_rt)) +
   geom_point() +
-  geom_smooth(method = "lm")
+  geom_smooth(method = 'lm') +
+  facet_wrap( ~ participant)
 
-# repeat the plot with facet wraps for participants
 d |> 
-  filter(participant %in% ids$participant) |>
-  ggplot(aes(x = scaled_word_familiarity, y = resp.rt)) +
+  filter(keep_word) |> 
+  ggplot(aes(participant_vocabulary_size,log_rt)) +
   geom_point() +
-  geom_smooth(method = "lm") +
-  facet_wrap(~ participant)
+  geom_smooth(method = 'lm') +
+  facet_wrap( ~ word)
 
-# -- modelling: word familiarity and vocabulary size -- #
+# -- aggregate -- #
 
-# fit a linear model on d predicting resp.rt from scaled_word_familiarity, assign to lm1
-lm1 = lm(resp.rt ~ scaled_word_familiarity, data = d)
+## participants
 
-# take lm1, get a tidy table of coefficient estimates with confidence intervals
-tidy(lm1, conf.int = TRUE)
+participants = d |> 
+  group_by(participant,participant_vocabulary_size) |> 
+  summarise(mean_participant_rt = mean(log_rt))
 
-# take d, group by word and scaled word familiarity, summarise mean resp.rt as mean_rt, assign table to dw
-dw = d |> 
-  group_by(word, scaled_word_familiarity) |>
-  summarise(mean_rt = mean(resp.rt))
+participants |> 
+  ggplot(aes(participant_vocabulary_size,mean_participant_rt)) +
+  geom_point()
 
-# take dw, plot mean_rt across scaled_word_familiarity with points and a line
-dw |> 
-  ggplot(aes(x = scaled_word_familiarity, y = mean_rt)) +
-  geom_point() +
-  geom_smooth(method = "lm")
+plm1 = lm(log_rt ~ participant_vocabulary_size, data = d)
+plm2 = lm(mean_participant_rt ~ participant_vocabulary_size, data = participants)
 
-dw |> 
-  ggplot(aes(x = scaled_word_familiarity, y = mean_rt, label = word)) +
-  geom_label() +
-  geom_smooth(method = "lm")
+tidy(plm1, conf.int = T)
+tidy(plm2, conf.int = T)
 
-# fit a second model on the aggregated data, assign to lm2
-lm2 = lm(mean_rt ~ scaled_word_familiarity, data = dw)
+## words
 
-# get a tidy table of coefficient estimates with confidence intervals
-tidy(lm2, conf.int = TRUE)
+# word, word_familiarity
 
-# take d, plot resp.rt ~ scaled participant vocabulary size with points and a line
-d |> 
-  ggplot(aes(x = scaled_participant_vocabulary_size, y = resp.rt)) +
-  geom_point() +
-  geom_smooth(method = "lm")
+# wlm1, wlm2
 
-# take d, bin scaled vocabulary size into four bins, plot resp.rt ~ scaled word familiarity with binned participant vocabulary size as colour, with points and a line
-d |> 
-  mutate(scaled_participant_vocabulary_size_binned = cut(scaled_participant_vocabulary_size, breaks = 4)) |>
-  ggplot(aes(x = scaled_word_familiarity, y = resp.rt, colour = scaled_participant_vocabulary_size_binned)) +
-  geom_point() +
-  geom_smooth(method = "lm")
+# -- mixed model -- #
 
-# repeat plot but remove points
-d |> 
-  mutate(scaled_participant_vocabulary_size_binned = cut(scaled_participant_vocabulary_size, breaks = 4)) |>
-  ggplot(aes(x = scaled_word_familiarity, y = resp.rt, colour = scaled_participant_vocabulary_size_binned)) +
-  geom_smooth(method = "lm")
+plm3 = lmer(log_rt ~ participant_vocabulary_size + (1|participant), data = d)
 
-# fit a linear model on d predicting resp.rt from scaled vocabulary size, assign to lm3
-lm3 = lm(resp.rt ~ scaled_participant_vocabulary_size, data = d)
+tidy(plm1, conf.int = T)
+tidy(plm2, conf.int = T)
+tidy(plm3, conf.int = T)
 
-# get a tidy table of coefficient estimates with confidence intervals
-tidy(lm3, conf.int = TRUE)
+# wlm3...
 
-# take d, group by participant and scaled vocabulary size, summarise mean resp.rt as mean_rt, assign table to dp
-dp = d |> 
-  group_by(participant, scaled_participant_vocabulary_size) |>
-  summarise(mean_rt = mean(resp.rt))
+# -- nested model -- #
 
-# make plot lol
-dp |> 
-  ggplot(aes(x = scaled_participant_vocabulary_size, y = mean_rt)) +
-  geom_point() +
-  geom_smooth(method = "lm")
+lmm1 = lmer(log_rt ~ participant_vocabulary_size + word_familiarity + (1|participant) + (1|word), data = d)
 
-# fit a second model on the aggregated data
-lm4 = lm(mean_rt ~ scaled_participant_vocabulary_size, data = dp)
+tidy(lmm1, conf.int = T)
 
-# get a tidy table of coefficient estimates with confidence intervals
-tidy(lm4, conf.int = TRUE)
+lmm2 = lmer(log_rt ~ participant_vocabulary_size * word_familiarity + (1|participant) + (1|word), data = d)
 
-# fit a linear model on d predicting resp.rt from scaled vocabulary size and scaled word familiarity
-lm5 = lm(resp.rt ~ scaled_participant_vocabulary_size + scaled_word_familiarity, data = d)
-
-# fit a linear model with an interaction
-lm6 = lm(resp.rt ~ scaled_participant_vocabulary_size * scaled_word_familiarity, data = d)
-
-# use compare performance to compare lm5 and lm6 and plot the result
-plot(compare_performance(lm5, lm6))
-
-# get a tidy table of coefficient estimates with confidence intervals for lm6
-tidy(lm6, conf.int = TRUE)
-
-# -- hierarchical modelling -- #
-
-# fit a hierarchical model on d predicting resp.rt from scaled and and scaled vocabulary size with a participant random intercept
-lm7 = lmer(resp.rt ~ scaled_participant_vocabulary_size + scaled_word_familiarity + (1 | participant), data = d)
-
-lm7 = lmer(resp.rt ~ 1 + scaled_participant_vocabulary_size + scaled_word_familiarity + (1 | participant), data = d)
-
-# fit the same model with an interaction
-lm8 = lmer(resp.rt ~ 1 + scaled_participant_vocabulary_size * scaled_word_familiarity + (1 | participant), data = d)
-
-# compare performance of the two models and plot it
-plot(compare_performance(lm7, lm8))
-
-# get a tidy table of coefficient estimates with confidence intervals for lm7 and lm8
-tidy(lm7, conf.int = TRUE)
-tidy(lm8, conf.int = TRUE)
-
-# get random effects from lm7 and plot them
-
-# fit a hierarchical model on d predicting resp.rt from scaled and and scaled vocabulary size with a participant and a word random intercept
-lm9 = lmer(resp.rt ~ 1 + scaled_participant_vocabulary_size + scaled_word_familiarity + (1 | participant) + (1 | word), data = d)
-
-tidy(lm9, conf.int = TRUE) |> 
-  select(term,estimate,std.error,statistic)
-
-# compare performance of the two models and plot it
-
-# plot random effects for lm9
+# health
+check_model(lmm1)
+check_model(lmm2)
+# diagnostics
+plot(compare_performance(lmm1,lmm2, metrics = 'common'))
+test_likelihoodratio(lmm1,lmm2)
+test_performance(lmm2,lmm1)
