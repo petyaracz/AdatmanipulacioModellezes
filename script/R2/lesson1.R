0734a613aea14b8367844e8872ebf853b75be562
petyaracz <raczpetya@migusz.local>
2026-09-10 12:32:39 +0200

    lesson1

--- script/R2/lesson1.R
+++ script/R2/lesson1.R
@@ -1,10 +1,13 @@
 # -- head -- #
 
-library(tidyverse)
-library(ggthemes)
-library(broom)
-library(performance)
+library(tidyverse) # adatmanipuláció, ábrák
+library(ggthemes) # ábrák
+library(broom) # adattáblák
+library(performance) # diagnosztika
 
+# -- maths -- #
+
+# how ordinary least squares work
 OLS = function(x, y) {
   # calculate the mean of x and y
   x_bar = mean(x)
@@ -20,6 +23,43 @@ OLS = function(x, y) {
   
   # return the regression coefficients
   return(c(b_0, b_1))
+}
+
+# how it compares to a Pearson correlation
+COR_from_OLS = function(x, y) {
+  x_bar = mean(x)
+  y_bar = mean(y)
+  
+  # same building blocks as OLS()
+  SS_xy = sum((x - x_bar) * (y - y_bar))
+  SS_xx = sum((x - x_bar) ^ 2)
+  SS_yy = sum((y - y_bar) ^ 2)
+  
+  # b_1, straight from OLS()
+  b_1 = SS_xy / SS_xx
+  
+  # r, direct definition
+  r_direct = SS_xy / sqrt(SS_xx * SS_yy)
+  
+  # r, recovered by rescaling b_1 (r = b * sd(x)/sd(y))
+  r_from_b = b_1 * sqrt(SS_xx) / sqrt(SS_yy)
+  
+  return(c(b_1 = b_1, r_direct = r_direct, r_from_b = r_from_b, R2 = r_direct^2))
+}
+
+# OLS on standardised vars is same as cor on anything
+OLS_standardised = function(x, y) {
+  # standardise both variables: mean 0, sd 1
+  x_std = (x - mean(x)) / sd(x)
+  y_std = (y - mean(y)) / sd(y)
+  
+  # feed into the existing OLS()
+  coefs = OLS(x_std, y_std)
+  
+  b_0_std = coefs[1]
+  b_1_std = coefs[2]
+  
+  return(c(b_0_std = b_0_std, b_1_std = b_1_std, r = cor(x, y)))
 }
 
 # -- en -- #
@@ -43,6 +83,8 @@ augment(lm)
 
 OLS(x,y)
 OLS(d$x,d$y)
+COR_from_OLS(x,y)
+OLS_standardised(x,y)
 
 d$pred = predict(lm)
 
